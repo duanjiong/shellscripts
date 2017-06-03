295360a1ed3bc7c18174d4ae441c82056c176a8d
Duan Jiong <jduan@fiberhome.com>
2017-06-03 10:43:08 +0800

    set repo env
    Signed-off-by: Duan Jiong <jduan@fiberhome.com>

---
+++ set_repo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source lib/common_function.sh
+
+
+#set screenshot
+if is_ubuntu ; then
+    sudo add-apt-repository ppa:dhor/myway
+    sudo apt-get update
+    sudo apt-get install hotshots﻿​
+fi
+
+if is_fedora; then
+    #更具不同的版本来选择
+    sudo wget http://download.opensuse.org/repositories/home:/zhonghuaren/Fedora_${os_RELEASE}/home:zhonghuaren.repo -O /etc/yum.repo.d/
+    sudo yum install hotshots
+fi
