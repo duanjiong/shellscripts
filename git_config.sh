e4021606447657540bda2f63a4dbd93e681cb341
Duan Jiong <jduan@fiberhome.com>
2017-06-03 16:54:09 +0800

    ...
    Signed-off-by: Duan Jiong <jduan@fiberhome.com>

--- git_config.sh
+++ git_config.sh
@@ -5,3 +5,16 @@
 #https://youtrack.jetbrains.com/issue/IDEA-64024
 
 git config fetch.recurseSubmodules yes
+git config --global core.editor vi
+
+#https://git-scm.com/book/zh/v1/%E8%B5%B7%E6%AD%A5-%E5%88%9D%E6%AC%A1%E8%BF%90%E8%A1%8C-Git-%E5%89%8D%E7%9A%84%E9%85%8D%E7%BD%AE
+echo global?
+read scope
+
+if [ $scope = "global" ];then
+    git config --global user.name "Duan Jiong"
+    git config --global user.email 380657134@qq.com
+elif
+    git config  user.name "Duan Jiong"
+    git config  user.email 380657134@qq.com
+fi
