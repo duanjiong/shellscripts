019817536de410bb4223810e37fa9712535d7181
Duan Jiong <jduan@fiberhome.com>
2017-06-03 11:02:21 +0800

    config git
    Signed-off-by: Duan Jiong <jduan@fiberhome.com>

--- git_config.sh
+++ git_config.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+#pycharm git submodule support
+#https://intellij-support.jetbrains.com/hc/en-us/community/posts/207069875-Git-submodule-support-
+#https://youtrack.jetbrains.com/issue/IDEA-64024
+
 git config fetch.recurseSubmodules yes
